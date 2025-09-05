d640ffcf8069003be7315a453c5ea4fd3c4a5359
Vjatsesalv Rovlin <vjatsesalv.rovlin@gmail.com>
2025-09-05 10:45:19 +0300

    math

---
+++ math.php
@@ -0,0 +1,34 @@
+<?php
+
+$bool = true && false;
+$bool = false && true;
+$bool = false && false;
+$bool = true && true;
+var_dump($bool);
+$bool = true || false;
+$bool = false || true;
+$bool = true || true;
+$bool = false || false;
+var_dump($bool);
+$bool = !false;
+var_dump($bool);
+$bool = !true;
+var_dump($bool);
+
+
+$bool = (true || false && true) && !(!false || false && true);
+var_dump($bool);
+$bool = false;
+
+$bool = 10 > 4;
+$bool = 10 < 4;
+$bool = 10 < 10;
+$bool = 10 > 10;
+$bool = 10 == 10;
+$bool = 10 == '10';
+$bool = 10 === '10';
+$bool = 10 != '10';
+$bool = 10 !== '10';
+$bool = 10 <= 10;
+$bool = 10 >= 10;
+var_dump($bool);
