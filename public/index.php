6f1829ae9038053e7bbdcaaedf2a40b4545f7442
sqtatdev <slava.ryvlin@gmail.com>
2025-09-26 11:41:04 +0300

    update controllesrs

--- public/index.php
+++ public/index.php
@@ -1,36 +1,57 @@
 <?php
+
 if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 }
 
-
-function dump(...$args)
-{
+function dump(...$args) {
     echo '<pre>';
     var_dump(...$args);
     echo '</pre>';
 }
 
-switch ($_SERVER['REQUEST_URI']) {
-    case '/':
-        $posts = [
-            ['title' => 'World news 1', 'published' => '26.09.25', 'author' => 'Kaspar', 'body' => 'Some world news 1'],
-            ['title' => 'World news 2', 'published' => '25.09.25', 'author' => 'Martin', 'body' => 'Some world news 2'],
-            ['title' => 'World news 3', 'published' => '24.09.25', 'author' => 'Pets', 'body' => 'Some world news 3'],
-            ['title' => 'World news 4', 'published' => '23.09.25', 'author' => 'Manivald', 'body' => 'Some world news 4'],
-        ];
-        include __DIR__ . '/../views/index.php';
-        break;
-    case '/us':
-        $posts = [
-            ['title' => 'U.S news 1', 'published' => '26.09.25', 'author' => 'Kaspar', 'body' => 'Some U.S news 1'],
-            ['title' => 'U.S news 2', 'published' => '25.09.25', 'author' => 'Martin', 'body' => 'Some U.S news 2'],
-            ['title' => 'U.S news 3', 'published' => '24.09.25', 'author' => 'Pets', 'body' => 'Some U.S news 3'],
-            ['title' => 'U.S news 4', 'published' => '23.09.25', 'author' => 'Manivald', 'body' => 'Some U.S news 4'],
-        ];
-        include __DIR__ . '/../views/us.php';
-        break;
-    default:
-        http_response_code(404);
-        echo '404 page not found';
-}
+spl_autoload_register(function($class) {
+    $class = substr($class, 4);
+    require_once __DIR__. "/../src/$class.php"; 
+});
+
+// require_once __DIR__. '/../src/Router.php';
+// require_once __DIR__. '/../src/DB.php';
+
+use App\Controllers\PublicController as PC;
+
+$router = new App\Router();
+dump($router);
+$db = new App\DB();
+dump($db);
+$controller = new PC();
+$controller = new PC();
+$controller = new PC();
+$controller = new PC();
+$controller = new PC();
+dump($controller);
+// switch ($_SERVER['REQUEST_URI']) {
+//     case '/': 
+//         $posts = [
+//             ['title' => "World news 1", "published" => '26.09.2025', 'author' => "Miron", 'body' => "Some kind of news1"],
+//             ['title' => "World news 1", "published" => '27.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news2"],
+//             ['title' => "World news 3", "published" => '28.09.2025', 'author' => "Miron", 'body' => "Some kind of news3"],
+//             ['title' => "World news 4", "published" => '29.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news4"],
+//             ['title' => "World news 5", "published" => '30.09.2025', 'author' => "Miron", 'body' => "Some kind of news5"],
+//         ];
+//         include __DIR__ . '/../views/index.php';
+//         break;
+//     case '/us':
+//         $posts = [
+//             ['title' => "U.S news 1", "published" => '26.09.2025', 'author' => "Miron", 'body' => "Some kind of news1"],
+//             ['title' => "U.S news 2", "published" => '27.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news2"],
+//             ['title' => "U.S news 3", "published" => '28.09.2025', 'author' => "Miron", 'body' => "Some kind of news3"],
+//             ['title' => "U.S news 4", "published" => '29.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news4"],
+//             ['title' => "U.S news 5", "published" => '30.09.2025', 'author' => "Miron", 'body' => "Some kind of news5"],
+//         ];
+//         include __DIR__ . '/../views/us.php';
+//         break;
+//     default: 
+//         http_response_code(404);
+//         echo '404 page not found';
+// }
