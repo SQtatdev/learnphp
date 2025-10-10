fb91f6723eaaa0f0b2f4b035413fc1eb09e2452c
Vjatseslav Ryvlin <168544924+SQtatdev@users.noreply.github.com>
2025-10-10 11:35:45 +0300

    Add files via upload

---
+++ helpers.php
@@ -0,0 +1,21 @@
+<?php
+
+function dump(...$args) {
+    echo '<pre>';
+    var_dump(...$args);
+    echo '</pre>';
+}
+
+function dd(...$args) {
+    dump(...$args);
+    die;
+}
+
+function view($viewName, $variables=[]) {
+    extract($variables);
+    include __DIR__ . "/views/$viewName.php";
+}
+
+function redirect($path) {
+    header("Location: $path");
+}
--- public/index.php
+++ public/index.php
@@ -1,57 +1,29 @@
 <?php
-
 if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 }
 
-function dump(...$args) {
-    echo '<pre>';
-    var_dump(...$args);
-    echo '</pre>';
-}
 
-spl_autoload_register(function($class) {
+spl_autoload_register(function ($class){
     $class = substr($class, 4);
-    require_once __DIR__. "/../src/$class.php"; 
+    require_once __DIR__ . "/../src/$class.php";
 });
 
-// require_once __DIR__. '/../src/Router.php';
-// require_once __DIR__. '/../src/DB.php';
+require __DIR__ . '/../helpers.php';
+require __DIR__ . '/../routes.php';
 
-use App\Controllers\PublicController as PC;
-
-$router = new App\Router();
-dump($router);
-$db = new App\DB();
-dump($db);
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-dump($controller);
-// switch ($_SERVER['REQUEST_URI']) {
-//     case '/': 
-//         $posts = [
-//             ['title' => "World news 1", "published" => '26.09.2025', 'author' => "Miron", 'body' => "Some kind of news1"],
-//             ['title' => "World news 1", "published" => '27.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news2"],
-//             ['title' => "World news 3", "published" => '28.09.2025', 'author' => "Miron", 'body' => "Some kind of news3"],
-//             ['title' => "World news 4", "published" => '29.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news4"],
-//             ['title' => "World news 5", "published" => '30.09.2025', 'author' => "Miron", 'body' => "Some kind of news5"],
-//         ];
-//         include __DIR__ . '/../views/index.php';
-//         break;
-//     case '/us':
-//         $posts = [
-//             ['title' => "U.S news 1", "published" => '26.09.2025', 'author' => "Miron", 'body' => "Some kind of news1"],
-//             ['title' => "U.S news 2", "published" => '27.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news2"],
-//             ['title' => "U.S news 3", "published" => '28.09.2025', 'author' => "Miron", 'body' => "Some kind of news3"],
-//             ['title' => "U.S news 4", "published" => '29.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news4"],
-//             ['title' => "U.S news 5", "published" => '30.09.2025', 'author' => "Miron", 'body' => "Some kind of news5"],
-//         ];
-//         include __DIR__ . '/../views/us.php';
-//         break;
-//     default: 
-//         http_response_code(404);
-//         echo '404 page not found';
-// }
+$router = new App\Router($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+$match = $router->match();
+if($match){
+    if(is_callable($match['action'])){
+        call_user_func($match['action']);
+    } else if (is_array($match['action'])) {
+        $class = $match['action'][0];
+        $controller = new $class();
+        $method = $match['action'][1];
+        $controller->$method();
+    }
+    
+} else {
+    echo '<img src="https://http.cat/404">';
+}
